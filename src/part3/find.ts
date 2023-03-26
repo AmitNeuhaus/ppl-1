4e5052670aa5b7e02ee85e2572ada9e51515d2e6
amit neuhaus <amit@beehero.io>
2023-03-26 20:56:59 +0300

    finished task 3

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -1,25 +1,47 @@
 import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
+import * as R from 'ramda';
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
-    for (let i = 0; i < a.length; i++) {
-        if (pred(a[i])) return a[i];
-    }
-    throw "No element found.";
-}
+  for (let i = 0; i < a.length; i++) {
+    if (pred(a[i])) return a[i];
+  }
+  throw 'No element found.';
+};
 
-export const findResult : undefined = undefined;
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => {
+  if (a.length === 0) {
+    return makeFailure('Array is Empty');
+  }
+  const [head, rest] = R.splitAt(1, a);
+  if (pred(head[0])) {
+    return makeOk(head[0]);
+  } else {
+    return findResult(pred, rest);
+  }
+};
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
-    try {
-        const x = findOrThrow(x => x % 2 === 0, a);
-        return x * x;
-    } catch (e) {
-        return -1;
-    }
-}
+  try {
+    const x = findOrThrow((x) => x % 2 === 0, a);
+    return x * x;
+  } catch (e) {
+    return -1;
+  }
+};
 
-export const returnSquaredIfFoundEven_v2 : undefined = undefined;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => {
+  return bind(
+    findResult((x) => x % 2 === 0, a),
+    (x) => makeOk(x * x)
+  );
+};
 
-export const returnSquaredIfFoundEven_v3 : undefined = undefined;
+export const returnSquaredIfFoundEven_v3 = (a: number[]):number => {
+    return either(
+      findResult((x) => x % 2 === 0, a),
+      (x) => x*x,
+      () => -1,
+    );
+  };;
